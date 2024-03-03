7ae36bda7c200fede47d65fd29f040db66ebee30
Arnau <arnauapps@gmail.com>
2024-03-03 23:13:40 +0100

    Created PostsIndex test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return inertia('Posts/Index', []);
     }
 
     /**
---
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Controllers\PostController;
+
+use Inertia\Testing\AssertableInertia;
+use function Pest\Laravel\get;
+
+it('should return the correct component', function() {
+    get(route('posts.index'))
+    ->assertInertia(fn (AssertableInertia $inertia) => $inertia
+        ->component('Posts/Index', true)
+    );
+});
