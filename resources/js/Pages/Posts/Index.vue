5095f0d8d5e13cc023e6008efd952decc7e6d876
Arnau <arnauapps@gmail.com>
2024-03-06 23:32:06 +0100

    Created Pagination for posts. Not completely functional yet.

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,16 +2,18 @@
     <AppLayout>
         <Container>
             <ul class="divide-y">
-                <li class="px-2 py-4" v-for="post in posts" :key="post.id">
+                <li class="px-2 py-4" v-for="post in posts.data" :key="post.id">
                     <span class="font-bold text-lg">{{ post.title }}</span>
                 </li>
             </ul>
+            <Pagination :meta="posts.meta" />
         </Container>
     </AppLayout>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
 defineProps({
     posts: Array,
 });
