b602f389aeeacb9e50f57bbf1120605bcfc11789
Arnau <arnauapps@gmail.com>
2024-03-06 23:51:50 +0100

    Pagination works. Link tag does not change data though.

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -36,59 +36,24 @@
             </div>
             <div>
                 <nav
-                    class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                    class="isolate inline-flex -space-x-px rounded-md shadow-sm bg-white"
                     aria-label="Pagination"
                 >
                     <a
-                        href="#"
-                        class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                        v-for="link in meta.links"
+                        :key="link.label"
+                        :href="link.url"
+                        class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2"
+                        :class="{
+                            'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600':
+                                link.active,
+                            'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0':
+                                !link.active,
+                        }"
+                        v-html="link.label"
                     >
-                        <span class="sr-only">Previous</span>
-                        <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
                     </a>
                     <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
-                    <a
-                        href="#"
-                        aria-current="page"
-                        class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >1</a
-                    >
-                    <a
-                        href="#"
-                        class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >2</a
-                    >
-                    <a
-                        href="#"
-                        class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                        >3</a
-                    >
-                    <span
-                        class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
-                        >...</span
-                    >
-                    <a
-                        href="#"
-                        class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                        >8</a
-                    >
-                    <a
-                        href="#"
-                        class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >9</a
-                    >
-                    <a
-                        href="#"
-                        class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >10</a
-                    >
-                    <a
-                        href="#"
-                        class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                    >
-                        <span class="sr-only">Next</span>
-                        <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-                    </a>
                 </nav>
             </div>
         </div>
@@ -97,31 +62,8 @@
 
 <script setup>
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
+import { Link } from "@inertiajs/vue3";
 defineProps({
     meta: Array,
 });
-
-const items = [
-    {
-        id: 1,
-        title: "Back End Developer",
-        department: "Engineering",
-        type: "Full-time",
-        location: "Remote",
-    },
-    {
-        id: 2,
-        title: "Front End Developer",
-        department: "Engineering",
-        type: "Full-time",
-        location: "Remote",
-    },
-    {
-        id: 3,
-        title: "User Interface Designer",
-        department: "Design",
-        type: "Full-time",
-        location: "Remote",
-    },
-];
 </script>
