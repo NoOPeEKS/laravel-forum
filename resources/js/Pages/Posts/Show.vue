03f214e2f0e81784761532d8aec8f9558679781f
Arnau <arnauapps@gmail.com>
2024-03-09 21:04:22 +0100

    Styled posts.show route

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,14 +1,19 @@
 <template>
     <AppLayout :title="post.title">
-        <h1>{{ post.title }}</h1>
-        <span>{{ formattedDate }} ago by {{ post.user.name }}</span>
-        <article>
-            {{ post.body }}
-        </article>
+        <Container>
+            <h1 class="text-2xl font-bold">{{ post.title }}</h1>
+            <span class="block mt-1 text-sm text-gray-600"
+                >{{ formattedDate }} ago by {{ post.user.name }}</span
+            >
+            <article class="mt-6">
+                {{ post.body }}
+            </article>
+        </Container>
     </AppLayout>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
 import { formatDistance, parseISO } from "date-fns";
 import { computed } from "vue";
 const props = defineProps({
