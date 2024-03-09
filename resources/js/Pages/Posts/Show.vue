3f136aa6c3a849099bc69cd07dd27bd93ddce15b
Arnau <arnauapps@gmail.com>
2024-03-09 23:20:05 +0100

    Comments bodies are visible on post.show route

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -8,16 +8,38 @@
             <article class="mt-6">
                 <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
             </article>
+            <div class="mt-12">
+                <h2 class="text-xl font-semibold">Comments</h2>
+                <ul class="divide-y mt-4">
+                    <li
+                        v-for="comment in comments.data"
+                        :key="comment.id"
+                        class="block px-2 py-4"
+                    >
+                        <span class="text-sm">
+                            {{ comment.body }}
+                        </span>
+                        <span
+                            class="first-letter:uppercase block pt-1 text-sm text-gray-600"
+                        >
+                            By Joe Bloggs
+                        </span>
+                    </li>
+                </ul>
+                <Pagination :meta="comments.meta" />
+            </div>
         </Container>
     </AppLayout>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
 import { formatDistance, parseISO } from "date-fns";
 import { computed } from "vue";
 const props = defineProps({
     post: Object,
+    comments: Object,
 });
 
 const formattedDate = computed(() =>
