3b6b8d7dba9e5c7a07379324930c04d89354a5d7
Arnau <arnauapps@gmail.com>
2024-03-09 22:38:55 +0100

    Added user relationship to show post user in posts.index route

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -5,6 +5,10 @@
                 <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                     <Link :href="route('posts.show', post.id)">
                         <span class="font-bold text-lg">{{ post.title }}</span>
+                        <span class="block mt-1 text-sm text-gray-600"
+                            >{{ formattedDate(post) }} ago by
+                            {{ post.user.name }}</span
+                        >
                     </Link>
                 </li>
             </ul>
@@ -16,8 +20,13 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
+import { formatDistance, parseISO } from "date-fns";
 import { Link } from "@inertiajs/vue3";
 defineProps({
     posts: Object,
 });
+
+const formattedDate = (post) => {
+    return formatDistance(parseISO(post.created_at), new Date());
+};
 </script>
