f82463c95fd6eda688a33d6d87925c4fd987f931
Arnau <arnauapps@gmail.com>
2024-03-10 23:45:24 +0100

    Created first iteration of posting comments frontend

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -10,6 +10,22 @@
             </article>
             <div class="mt-12">
                 <h2 class="text-xl font-semibold">Comments</h2>
+                <form @submit.prevent="addComment" class="mt-4">
+                    <div>
+                        <InputLabel for="body" class="sr-only">
+                            Comment
+                        </InputLabel>
+                        <TextArea
+                            id="body"
+                            v-model="commentForm.body"
+                            placeholder="Speak your mind..."
+                            rows="4"
+                        />
+                    </div>
+                    <PrimaryButton type="submit" class="mt-3">
+                        Add Comment
+                    </PrimaryButton>
+                </form>
                 <ul class="divide-y mt-4">
                     <li
                         v-for="comment in comments.data"
@@ -29,12 +45,25 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
 import Comment from "@/Components/Comment.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextArea from "@/Components/TextArea.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { relativeDate } from "@/Utilities/date.js";
 import { computed } from "vue";
+import { useForm } from "@inertiajs/vue3";
 const props = defineProps({
     post: Object,
     comments: Object,
 });
 
 const formattedDate = computed(() => relativeDate(props.post.created_at));
+const commentForm = useForm({
+    body: "",
+});
+const addComment = () => {
+    commentForm.post(route("posts.comments.store", props.post.id), {
+        preserveScroll: true,
+        onSuccess: () => commentForm.reset(),
+    });
+};
 </script>
